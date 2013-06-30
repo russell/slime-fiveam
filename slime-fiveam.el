8349e5403f4219aae493c581361eae71f3e11745
Russell Sim <russell.sim@gmail.com>
2013-06-30 12:13:54 +1000

    Fixed license.

--- slime-fiveam.el
+++ slime-fiveam.el
@@ -1,19 +1,20 @@
 ;; SLIME FiveAM integration.
 ;; Copyright (C) 2013 Russell Sim <russell.sim@gmail.com>
 ;;
-;; This program is free software: you can redistribute it and/or
-;; modify it under the terms of the GNU Lesser General Public License
-;; as published by the Free Software Foundation, either version 3 of
-;; the License, or (at your option) any later version.
+;; This file is part of GNU Emacs.
 ;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; Lesser General Public License for more details.
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 ;;
-;; You should have received a copy of the GNU Lesser General Public
-;; License along with this program.  If not, see
-;; <http://www.gnu.org/licenses/>.
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 (eval-when-compile
   (require 'slime)
